fe31898efd17c1058737f04a76d172e71490cd4e
Vikil Naik <vikilnaik37@gmail.com>
2019-02-19 14:40:58 -0500

    products is finished, stil working on shopping cart

--- products.py
+++ products.py
@@ -9,7 +9,7 @@ class Products:
     def __str__(self):
         return "This is your product: {}. The base price is: {}, the tax rate is: {:.2f}" .format(self.name, self.base_price, self.tax)
 
-    def price(self, base_price, tax):
+    def total_price(self, base_price, tax):
         price = base_price * tax
         total = price + base_price
         return total
@@ -17,4 +17,4 @@ class Products:
 
 product1 = Products("apple", 5, 0.25)
 print(product1)
-print("And the total price of this product is: {}".format(product1.price(50, 0.25)))
+print("And the total price of this product is: {}".format(product1.total_price(50, 0.25)))
--- shopping_cart.py
+++ shopping_cart.py
@@ -1,13 +1,25 @@
 
-from product import Products
+from products import Products
 
 class ShoppingCart:
 
-    def __init__(self, product):
+    def __init__(self, products):
         self.products = products
 
     def add (self, product):
-        self.products.append(product)
+        self.products.append(products)
 
     def remove(self, product):
-        self.products.remove(product)    
+        self.products.remove(products)
+
+    def before_tax_total(self):
+        before_tax = 0
+        for product in self.products:
+            before_tax += products.base_price
+        return before_tax
+
+    def after_tax_total(self):
+        after_tax = 0
+        for product in self.prodcuts:
+            after_tax += products.price()
+        return after_tax
